1c297637f8cf611af289a709785d64a0d4ef3aa0
pwirth <wirth@hallowelt.biz>
2015-04-21 13:39:23 +0200

    BS Notifications: Fixes/Improvements/Standardizations
    * Added formatter and decorator classes to use own intro and footer
    * Used own formatting also for mw mails
    * Added realname param
    * bs-newuser is (really) only for admins
    * bs-newuser works also on MW AddNewAccount
    * bs-newuser user setting is hidden for non-admins
    * Used GENDER destinction for messages
    * Added real name params to every mail
    * Fixed some messages in de, de-formal and en
    * Fixed qqq
    * Fixed some Notices
    * Added de-fromal message for echo default footer
    * Deaktivated mention, page-linked and reverted notifications-did not work
    * Deaktivated batch mails - currently wont work in BS
    => This will kind a do for next release. TODO: crlt+a, entf, re-write
    
    Change-Id: Id5037ba1999174c4837153c42896ee41d3604354

--- Notifications/Notifications.setup.php
+++ Notifications/Notifications.setup.php
@@ -2,13 +2,20 @@
 
 BsExtensionManager::registerExtension('Notifications', BsRUNLEVEL::FULL|BsRUNLEVEL::REMOTE);
 
+//Do not enable Batch mails - Wont work for BS so far
+$wgEchoEnableEmailBatch = false;
+
 // MessageFiles
 $wgMessagesDirs['Notifications'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['Notifications'] = __DIR__ . '/Notifications.i18n.php';
 
 // Autoloader
 $GLOBALS['wgAutoloadClasses']['Notifications'] = __DIR__ . '/Notifications.class.php';
+
 $wgAutoloadClasses['BsNotificationsFormatter'] = __DIR__.'/includes/BsNotificationsFormatter.class.php';
+$wgAutoloadClasses['BsEchoTextEmailFormatter'] = __DIR__.'/includes/BsEchoTextEmailFormatter.class.php';
+$wgAutoloadClasses['BsEchoEmailSingle'] = __DIR__.'/includes/BsEchoEmailSingle.class.php';
+$wgAutoloadClasses['BsEchoTextEmailDecorator'] = __DIR__.'/includes/BsEchoTextEmailDecorator.class.php';
 
 $wgResourceModuleTemplate = array(
 	'localBasePath' => "$IP/extensions/BlueSpiceExtensions/Notifications/resources",
@@ -39,5 +46,8 @@ $wgDefaultUserOptions['echo-subscriptions-email-mention'] = false;
 // Web
 $wgDefaultUserOptions['echo-subscriptions-web-article-linked'] = true;
 $wgDefaultUserOptions['echo-subscriptions-web-mention'] = true;
+
+//Overwrite, cause there is no hook
+$wgEchoNotifiers['web'] = array( 'Notifications', 'notifyWithNotification' );
 
 unset( $wgResourceModuleTemplate );
