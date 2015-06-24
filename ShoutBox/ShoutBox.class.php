58b8063fd9a1a9fe90fefd247f9b9b157571c7e5
Frederic Mohr <frederic.mohr@gmail.com>
2015-06-24 08:54:36 +0200

    SECURITY: Fixed XSS vulnerability in Shoutbox
    The ShoutBox extension in the BlueSpice Enterprise Wiki contained a stored
    XSS flaw. Entered data was stored, retrieved and displayed unsanitized,
    which allowed for injection of JavaScript.
    
    Added encoding of all data input before storing it in the database.
    
    Change-Id: I02b6e59ae57b83b194de33a270a78364adc22aa4

--- ShoutBox/ShoutBox.class.php
+++ ShoutBox/ShoutBox.class.php
@@ -336,6 +336,7 @@ class ShoutBox extends BsExtensionMW {
 		if ( strlen( $sMessage ) > BsConfig::get( 'MW::ShoutBox::MaxMessageLength' ) ) {
 			$sMessage = substr( $sMessage, 0, BsConfig::get( 'MW::ShoutBox::MaxMessageLength' ) );
 		}
+		$sMessage = htmlspecialchars( $sMessage, ENT_QUOTES, 'UTF-8' );
 
 		// TODO MRG (08.09.10 01:57): error message
 		if ( $iArticleId <= 0 ) return false;
