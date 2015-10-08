7942d01b12d4c6a58a97a63db97e32826cd0a897
dvogel <daniel.vogel@hallowelt.com>
2015-10-08 12:14:07 +0200

    Fixed issue with positioning TopMenuBar childs
    Change-Id: Idc24aefb6a8d57482f409ab66fccf15412b42113

--- TopMenuBarCustomizer/resources/bluespice.TopMenuBarCustomizer.js
+++ TopMenuBarCustomizer/resources/bluespice.TopMenuBarCustomizer.js
@@ -9,21 +9,24 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v2 or later
  * @filesource
  */
-
 $(document).ready(function(){
-    $('.menu-item-container').hover( function(){
-			$(this).siblings('ul.bs-apps-child').stop(true,true).slideDown('fast');
+	$('.menu-item-container').hover( function(){
+			if( !$(this).hasClass('level-1') ) {
+				$(this).next('ul').css('top', ($(this).position().top) + 'px');
+				$(this).next('ul').css('left', ($(this).position().left + $(this).outerWidth()) + 'px');
+			}
+			$(this).next('ul').stop(true,true).fadeIn('fast');
 		},
 		function(){
-			$(this).siblings('ul.bs-apps-child').stop(true,true).delay(100).slideUp('fast');
+			$(this).siblings('ul.bs-apps-child').stop(true,true).delay(100).fadeOut('fast');
 		}
-	)
+	);
 
 	$('ul.bs-apps-child').hover( function(){
-			$(this).stop(true,true).slideDown('fast');
+			$(this).stop(true,true).fadeIn('fast');
 		},
 		function(){
-			$(this).stop(true,true).delay(100).slideUp('fast');
+			$(this).stop(true,true).delay(100).fadeOut('fast');
 		}
-	)
+	);
 });
