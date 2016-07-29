bc3754926628f067e33b26fa4541d6ccdca738a5
pwirth <wirth@hallowelt.biz>
2016-07-29 16:18:07 +0200

    BSAvatars: Using the new extension registration
    Change-Id: I340b958bcf43029595aad52190e43f9f84f705b8

--- Avatars/Avatars.setup.php
+++ Avatars/Avatars.setup.php
@@ -1,34 +1,6 @@
 <?php
 
-BsExtensionManager::registerExtension('Avatars', BsRUNLEVEL::FULL | BsRUNLEVEL::REMOTE, BsACTION::LOAD_SPECIALPAGE);
-
-$wgMessagesDirs['Avatars'] = __DIR__ . '/i18n';
-
-$wgAutoloadClasses['Avatars'] = __DIR__ . '/Avatars.class.php';
-
-$wgHooks['BeforePageDisplay'][] = "Avatars::onBeforePageDisplay";
-
-$wgResourceModules['ext.bluespice.avatars.js'] = array(
-	'scripts' => array(
-		'extensions/BlueSpiceExtensions/Avatars/resources/bluespice.avatars.js',
-	),
-	'messages' => array(
-		'bs-avatars-upload-title',
-		'bs-avatars-upload-label',
-		'bs-avatars-generate-new-label',
-		'bs-avatars-warning-title',
-		'bs-avatars-warning-text',
-		'bs-avatars-userimage-title',
-		'bs-avatars-set-userimage-failed',
-		'bs-avatars-set-userimage-saved',
-		'bs-avatars-userimage-help',
-		'bs-avatars-file-upload-fieldset-title',
-		'bs-avatars-userimage-title',
-		'bs-avatars-auto-generate-fieldset-title'
-	),
-	'localBasePath' => $IP,
-	'remoteBasePath' => &$GLOBALS['wgScriptPath']
-);
+wfLoadExtension( 'BlueSpiceExtensions/Avatars' );
 
 $wgForeignFileRepos[] = array(
 	'class' => 'FSRepo',
@@ -37,7 +9,3 @@ $wgForeignFileRepos[] = array(
 	'hashLevels' => 0,
 	'url' => BS_DATA_PATH . '/Avatars',
 );
-
-$wgAjaxExportList[] = 'Avatars::uploadFile';
-$wgAjaxExportList[] = 'Avatars::generateAvatarAjax';
-$wgAjaxExportList[] = 'Avatars::setUserImage';
