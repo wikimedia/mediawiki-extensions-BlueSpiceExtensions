0d957b90a22ada0b18d1e1d8455882225639c430
Leonid Verhovskij <l.verhovskij@gmail.com>
2016-08-10 11:24:06 +0200

    GroupManager: move to special page and register own permission improve msgkey usage as suggested in comments
    Change-Id: I266ef4bc918824e44f2cd18c206b326fd61a5df9

---
+++ GroupManager/includes/specials/SpecialGroupManager.class.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Special page for GroupManager of BlueSpice (MediaWiki)
+ *
+ * Part of BlueSpice for MediaWiki
+ *
+ * @author     Leonid Verhovskij <verhovskij@hallowelt.com>
+ * @version    $Id$
+ * @package    BlueSpiceExtensions
+ * @subpackage GroupManager
+ * @copyright  Copyright (C) 2016 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License v2 or later
+ * @filesource
+ */
+class SpecialGroupManager extends BsSpecialPage {
+
+	public function __construct() {
+		parent::__construct( 'GroupManager', 'groupmanager-viewspecialpage' );
+	}
+
+	public function execute( $par ) {
+		parent::execute( $par );
+		$oOutputPage = $this->getOutput();
+
+		$this->getOutput()->addModules( 'ext.bluespice.groupManager' );
+		$oOutputPage->addHTML( '<div id="bs-groupmanager-grid"></div>' );
+	}
+
+	protected function getGroupName() {
+		return 'bluespice';
+	}
+
+}
