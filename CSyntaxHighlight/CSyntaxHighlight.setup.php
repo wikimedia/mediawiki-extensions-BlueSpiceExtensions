d111b0f72297898ea47d49acb1a1fdb19ce99f89
Gerhard Diller <gerharddiller@icloud.com>
2016-08-16 16:41:54 +0200

    CSyntaxHighlight: Using new extension.json
    Change-Id: Ie45ed13a170e8a3784432095a53df8fbc4825519

--- CSyntaxHighlight/CSyntaxHighlight.setup.php
+++ CSyntaxHighlight/CSyntaxHighlight.setup.php
@@ -1,7 +1,3 @@
 <?php
 
-BsExtensionManager::registerExtension('CSyntaxHighlight', BsRUNLEVEL::FULL|BsRUNLEVEL::REMOTE);
-
-$wgAutoloadClasses['CSyntaxHighlight'] = __DIR__ . '/CSyntaxHighlight.class.php';
-
-$wgMessagesDirs['CSyntaxHighlight'] = __DIR__ . '/i18n';
+wfLoadExtension( 'BlueSpiceExtensions/CSyntaxHighlight' );
