d95e76fb9fbddb9d766a33e3d86166c3b7025fac
Gerhard Diller <gerharddiller@icloud.com>
2016-08-17 14:30:39 +0200

    RSSStandards: using extension.json
    Patch Set 2: whitespace, authors, resource path
    
    Change-Id: Ia573a89ba1a7be7f23a9597bdd618624ac6c86e6

--- RSSStandards/RSSStandards.setup.php
+++ RSSStandards/RSSStandards.setup.php
@@ -1,19 +1,2 @@
 <?php
-
-BsExtensionManager::registerExtension('RSSStandards', BsRUNLEVEL::FULL|BsRUNLEVEL::REMOTE);
-
-$wgAutoloadClasses['RSSStandards'] = __DIR__ . '/RSSStandards.class.php';
-$wgAutoloadClasses['ApiRSSStandardsPagesStore'] = __DIR__ . '/includes/api/ApiRSSStandardsPagesStore.php';
-
-$wgMessagesDirs['RSSStandards'] = __DIR__ . '/i18n';
-
-$wgAPIModules['bs-rss-standards-pages-store'] = 'ApiRSSStandardsPagesStore';
-
-$wgResourceModules['ext.bluespice.rssStandards'] = array(
-	'scripts' => 'bluespice.rssStandards.js',
-	'localBasePath' => $IP . '/extensions/BlueSpiceExtensions/RSSStandards/resources',
-	'remoteExtPath' => 'BlueSpiceExtensions/RSSStandards/resources',
-	'dependencies' => array(
-		'ext.bluespice.extjs'
-	)
-);
+wfLoadExtension( 'BlueSpiceExtensions/RSSStandards' );
