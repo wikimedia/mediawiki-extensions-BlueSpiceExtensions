3cee30d9e30d6e74c9c9dac71217e4c99edcda39
Gerhard Diller <gerharddiller@icloud.com>
2016-08-17 16:36:42 +0200

    TopMenuBarCustomizer: using extension.json
    Change-Id: I4393d74da948168ddd414d3758b57af0b6a8e80c

--- TopMenuBarCustomizer/TopMenuBarCustomizer.setup.php
+++ TopMenuBarCustomizer/TopMenuBarCustomizer.setup.php
@@ -1,28 +1,2 @@
 <?php
-BsExtensionManager::registerExtension( 'TopMenuBarCustomizer', BsRUNLEVEL::FULL|BsRUNLEVEL::REMOTE );
-
-$bsgTopMenuCustomizerCheckRead = true;
-
-$aResourceModuleTemplate = array(
-	'dependencies' => 'ext.bluespice',
-	'localBasePath' => "$IP/extensions/BlueSpiceExtensions/TopMenuBarCustomizer/resources",
-	'remoteExtPath' => 'BlueSpiceExtensions/TopMenuBarCustomizer/resources'
-);
-
-$wgMessagesDirs['TopMenuBarCustomizer'] = __DIR__."/i18n";
-
-$wgAutoloadClasses['TopMenuBarCustomizer'] = __DIR__ . '/TopMenuBarCustomizer.class.php';
-$wgAutoloadClasses['TopMenuBarCustomizerParser'] = __DIR__."/includes/TopMenuBarCustomizerParser.php";
-$wgAutoloadClasses['ViewTopMenuItem'] = __DIR__."/views/view.TopMenuItem.php";
-
-$wgResourceModules['ext.bluespice.topmenubarcustomizer'] = array(
-	'scripts' => 'bluespice.TopMenuBarCustomizer.js',
-	'position' => 'top',
-) + $aResourceModuleTemplate;
-
-$wgResourceModules['ext.bluespice.topmenubarcustomizer.styles'] = array(
-	'styles'  => 'bluespice.TopMenuBarCustomizer.css',
-	'position' => 'top',
-) + $aResourceModuleTemplate;
-
-unset($aResourceModuleTemplate);
+wfLoadExtension( 'BlueSpiceExtensions/TopMenuBarCustomizer' );
