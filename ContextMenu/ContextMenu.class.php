ce303906ea9fb0599ba89a9771462f96271c5635
Gerhard Diller <gerharddiller@icloud.com>
2016-08-17 10:25:59 +0200

    ContextMenu: using extension.json
    Patch Set 2: whitespace, license
    
    Change-Id: I6fbf4e6a421ea89753d298627d9efc8091ebce69

--- ContextMenu/ContextMenu.class.php
+++ ContextMenu/ContextMenu.class.php
@@ -37,28 +37,6 @@
 class ContextMenu extends BsExtensionMW {
 
 	/**
-	 * Contructor of the ContextMenu class
-	 */
-	public function __construct() {
-		wfProfileIn('BS::' . __METHOD__);
-		// Base settings
-		$this->mExtensionFile = __FILE__;
-		$this->mExtensionType = EXTTYPE::PARSERHOOK;
-		$this->mInfo = array(
-			EXTINFO::NAME => 'ContextMenu',
-			EXTINFO::DESCRIPTION => 'bs-contextmenu-desc',
-			EXTINFO::AUTHOR => 'Tobias Weichart',
-			EXTINFO::VERSION => 'default',
-			EXTINFO::STATUS => 'default',
-			EXTINFO::PACKAGE => 'default',
-			EXTINFO::URL => 'https://help.bluespice.com/index.php/ContextMenu',
-			EXTINFO::DEPS => array('bluespice' => '2.23.0')
-		);
-		$this->mExtensionKey = 'MW::ContextMenu';
-		wfProfileOut('BS::' . __METHOD__);
-	}
-
-	/**
 	 * Initialization of ContextMenu extension
 	 */
 	protected function initExt() {
