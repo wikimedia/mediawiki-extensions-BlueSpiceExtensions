f07c438ef8472a1e57b59390708158eda6e35725
Gerhard Diller <gerharddiller@icloud.com>
2016-08-18 10:32:15 +0200

    WatchList: using extension.json
    Patch Set 2: whitespace, license
    
    Change-Id: I78b97ddaaa7b368e3027ed645ab8e0fad406d91f

--- WatchList/WatchList.setup.php
+++ WatchList/WatchList.setup.php
@@ -1,7 +1,2 @@
 <?php
-
-BsExtensionManager::registerExtension('WatchList', BsRUNLEVEL::FULL|BsRUNLEVEL::REMOTE);
-
-$wgAutoloadClasses['WatchList'] = __DIR__ . '/WatchList.class.php';
-
-$wgMessagesDirs['BSWatchList'] = __DIR__ . '/i18n'; //TODO: Must not be WatchList, somehting's overriding here otherwise
+wfLoadExtension( 'BlueSpiceExtensions/WatchList' );
