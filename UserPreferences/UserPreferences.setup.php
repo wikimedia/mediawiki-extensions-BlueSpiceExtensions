14eedac5b9389bd727ddeb7fb4a70325d0ee90e0
Leonid Verhovskij <l.verhovskij@gmail.com>
2016-08-25 09:43:02 +0200

    UserPreferences: fix loader in setup file
    Change-Id: I0f48d1e9ca2402c934f4ce9885946b74a94b0532

--- UserPreferences/UserPreferences.setup.php
+++ UserPreferences/UserPreferences.setup.php
@@ -1,15 +1,4 @@
 <?php
 
-BsExtensionManager::registerExtension('UserPreferences', BsRUNLEVEL::FULL|BsRUNLEVEL::REMOTE, BsACTION::LOAD_SPECIALPAGE|BsACTION::LOAD_ON_API);
+wfLoadExtension( 'BlueSpiceExtensions/UserPreferences' );
 
-$wgAutoloadClasses['UserPreferences'] = __DIR__ . '/UserPreferences.class.php';
-
-$wgMessagesDirs['UserPreferences'] = __DIR__ . '/i18n';
-
-$wgHooks['UserLoadOptions'][] = 'UserPreferences::onUserLoadOptions';
-
-$wgResourceModules['ext.bluespice.userpreferences'] = array(
-	'styles' => 'bluespice.userpreferences.css',
-	'localBasePath' => __DIR__.'/resources',
-	'remoteExtPath' => 'BlueSpiceExtensions/UserPreferences/resources'
-);
