c728dead6106bf30e6532c482c257bb9741db7c4
pwirth <wirth@hallowelt.biz>
2016-11-21 16:20:26 +0100

    BSReview: Added steps_id to params, so a hook handler does not need to execute that havvy query again
    => needs cherry-pick to REL1_27 and back port to REL1_23
    
    Change-Id: If030ba5dde52b5ad27069ebe3f6fb15e3f1d3a53

--- Review/Review.class.php
+++ Review/Review.class.php
@@ -1249,7 +1249,7 @@ class Review extends BsExtensionMW {
 
 		$dbw->freeResult( $res );
 
-		$step_id = $row[ 'revs_id' ];
+		$oParams->stepid = $step_id = $row[ 'revs_id' ];
 		$initial_comment = $row[ 'revs_comment' ];
 
 		// update data
