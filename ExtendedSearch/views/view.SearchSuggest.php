df371c2478a75b6ee455028f79a096d025016114
nasty <konrad@hallowelt.com>
2017-01-16 15:54:15 +0100

    ExtendedSearch: readded create page from search
    Fixed issue from ERM4026
    
    Change-Id: I526fb0f897613d459fee4abaeb5877ff91645cae

--- ExtendedSearch/views/view.SearchSuggest.php
+++ ExtendedSearch/views/view.SearchSuggest.php
@@ -32,11 +32,24 @@ class ViewSearchSuggest extends ViewBaseElement {
 	public function execute( $aParam = false ) {
 		$sSearch             = str_replace( BsCore::getForbiddenCharsInArticleTitle(), '', $this->getOption( 'search' ) );
 		$oTitle              = Title::newFromText( $sSearch );
+
+		$aLinks = [];
+		if( !$oTitle->exists() ) {
+			$aLinks['bs-extendedsearch-suggest'] = [
+				'href' => $oTitle->getLocalURL(),
+				'title' => wfMessage( 'bs-extendedsearch-create-page', $oTitle->getPrefixedText() )->plain(),
+				'text' => wfMessage( 'bs-extendedsearch-create-page', $oTitle->getPrefixedText() )->plain()
+			];
+		}
+
+		Hooks::run( 'BSExtendedSearchSpecialPageTermLinks', [ &$aLinks ] );
+
+		$sCreatesuggest  = '<ul>';
+		$sCreatesuggest .= $this->renderList( $aLinks );
+
 		$sSearchUrlencoded   = urlencode( $sSearch );
 		$sSearchHtmlEntities = htmlentities( $sSearch, ENT_QUOTES, 'UTF-8' );
-		$sCreatesuggest      = '<ul>';
-
-		wfRunHooks( 'BSExtendedSearchAdditionalActions', array( &$sCreatesuggest, &$sSearchUrlencoded, &$sSearchHtmlEntities, &$oTitle ) );
+		Hooks::run( 'BSExtendedSearchAdditionalActions', array( &$sCreatesuggest, &$sSearchUrlencoded, &$sSearchHtmlEntities, &$oTitle ), '2.27' );
 
 		$sCreatesuggest .= '</ul>';
 		$sCreatesuggest .= '<br />';
@@ -44,4 +57,33 @@ class ViewSearchSuggest extends ViewBaseElement {
 		return $sCreatesuggest;
 	}
 
+	/**
+	 * Renders a given array to links in list items.
+	 *
+	 * @param $aLinks array An array of links.
+	 * @return string Returns the HTML from the given array.
+	 */
+	private function renderList( $aLinks ) {
+		$sResult = '';
+
+		foreach ($aLinks as $sId => $aItem) {
+			$sResult .= Html::rawElement(
+				'li',
+				array(
+				),
+				Html::element(
+					'a',
+					array(
+						'id' => $sId,
+						'title' => $aItem['title'],
+						'href' => $aItem['href'],
+						'text' => $aItem['text']
+					),
+					$aItem['text']
+				)
+			);
+		}
+
+		return $sResult;
+	}
 }
