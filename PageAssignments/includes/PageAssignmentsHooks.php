5d034cfd4ee908030d72b3a88475b52f69b54572
nasty <konrad@hallowelt.com>
2017-02-15 11:12:23 +0100

    PageAssignments: added hook listener for user delete and group delete/rename.
    Fix for issue ERM5343
    
    Change-Id: I27651884c06705b9339054c3944193547e97a5ad

--- PageAssignments/includes/PageAssignmentsHooks.php
+++ PageAssignments/includes/PageAssignmentsHooks.php
@@ -145,4 +145,61 @@ class PageAssignmentsHooks {
 		);
 		return true;
 	}
+
+	/**
+	 * Deletes all page assignments on user deleted.
+	 * @param UserManager $oUserManager
+	 * @param User $oUser
+	 * @param &$oStatus
+	 * @return bool
+	 */
+	public static function onBSUserManagerAfterDeleteUser( $oUserManager, $oUser, &$oStatus, $oPerformer ) {
+		$dbr = wfGetDB( DB_MASTER );
+		$dbr->delete(
+			'bs_pageassignments',
+			array(
+				'pa_assignee_key' => $oUser->getName(),
+				'pa_assignee_type' => 'user'
+			)
+		);
+		return true;
+	}
+
+	/**
+	 * Updates all page assignments on group name change.
+	 * @param string $sGroup
+	 * @param string $sNewGroup
+	 * @return bool
+	 */
+	public static function onBSGroupManagerGroupNameChanged( $sGroup, $sNewGroup ) {
+		$dbr = wfGetDB( DB_MASTER );
+		$dbr->update(
+			'bs_pageassignments',
+			array(
+				'pa_assignee_key' => $sNewGroup,
+			),
+			array(
+				'pa_assignee_key' => $sGroup,
+				'pa_assignee_type' => 'group'
+			)
+		);
+		return true;
+	}
+
+	/**
+	 * Deletes all page assignments on group deleted.
+	 * @param string $sGroup
+	 * @return bool
+	 */
+	public static function onBSGroupManagerGroupDeleted( $sGroup) {
+		$dbr = wfGetDB( DB_MASTER );
+		$dbr->delete(
+			'bs_pageassignments',
+			array(
+				'pa_assignee_key' => $sGroup,
+				'pa_assignee_type' => 'group'
+			)
+		);
+		return true;
+	}
 }
