d1826e706bcd9f1a59a94f517a67455762948ba2
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-03-08 16:29:19 +0100

    UserManager: API unit test
    ERM:#5818
    
    Change-Id: I5c940085fc7ab49d4f36c40a372d3708be72bffd

---
+++ UserManager/tests/phpunit/BSApiChangeableGroupStoreTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @group medium
+ * @group API
+ * @group BlueSpice
+ */
+class BSApiChangeableGroupStoreTest extends BSApiExtJSStoreTestBase {
+
+	protected $iFixtureTotal = 6;
+
+	protected function getStoreSchema() {
+		return [
+			'group_name' => [
+				'type' => 'string'
+			],
+			'additional_group' => [
+				'type' => 'boolean'
+			],
+			'displayname' => [
+				'type' => 'string'
+			]
+		];
+	}
+
+	protected function createStoreFixtureData() {
+		global $wgAddGroups, $wgRemoveGroups, $wgGroupsAddToSelf, $wgGroupsRemoveFromSelf;
+		$wgAddGroups['sysop'] = true;
+		$wgRemoveGroups['sysop'] = true;
+		$wgGroupsAddToSelf['sysop'] = true;
+		$wgGroupsRemoveFromSelf['sysop'] = true;
+
+		return 6;
+	}
+
+	public function provideSingleFilterData() {
+		return [
+			'Filter by group_name' => [ 'string', 'ct', 'group_name', 'auto', 1 ],
+			'Filter by additional_group' => ['boolean', 'eq', 'additional_group', false, 6]
+		];
+	}
+
+	public function provideMultipleFilterData() {
+		return [
+			'Filter by group_name and displayname' => [
+				[
+					[
+						'type' => 'string',
+						'comparison' => 'eq',
+						'field' => 'group_name',
+						'value' => 'bureaucrat'
+					],
+					[
+						'type' => 'string',
+						'comparison' => 'ct',
+						'field' => 'displayname',
+						'value' => 'Bur'
+					]
+				],
+				1
+			]
+		];
+	}
+
+	protected function getModuleName() {
+		return 'bs-usermanager-group-store';
+	}
+}
