2d43bbd4261ade09f6ac54050087aa5d14726788
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-03-22 15:57:19 +0100

    NamespaceManager: API Unittest
    Change-Id: I5b48ea65b2e5f996546787aa58f84e792d4000be

---
+++ NamespaceManager/tests/phpunit/BSApiNamespaceStoreTest.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * @group medium
+ * @group API
+ */
+class BSApiNamespaceStoreTest extends BSApiExtJSStoreTestBase {
+	protected $iFixtureTotal = 21;
+
+	protected function getStoreSchema () {
+		return [
+			'id' => [
+				'type' => 'integer'
+			],
+			'name' => [
+				'type' => 'string'
+			],
+			'isSystemNS' => [
+				'type' => 'boolean'
+			],
+			'isTalkNS' => [
+				'type' => 'boolean'
+			],
+			'pageCount' => [
+				'type' => 'integer'
+			],
+			'content' => [
+				'type' => 'boolean'
+			],
+			'subpages' => [
+				'type' => 'boolean'
+			],
+			'searched' => [
+				'type' => 'boolean'
+			],
+		];
+	}
+
+	protected function createStoreFixtureData () {
+		return 21;
+	}
+
+	protected function getModuleName () {
+		return 'bs-namespace-store';
+	}
+
+	public function provideSingleFilterData() {
+		return [
+			'Filter by isSystemNS' => [ 'boolean', 'eq', 'isSystemNS', false, 3 ]
+		];
+	}
+
+	public function provideMultipleFilterData () {
+		return [
+			'Filter by subpages and searched' => [
+				[
+					[
+						'type' => 'boolean',
+						'comparison' => 'eq',
+						'field' => 'subpages',
+						'value' => true
+					],
+					[
+						'type' => 'boolean',
+						'comparison' => 'eq',
+						'field' => 'searched',
+						'value' => true
+					]
+				],
+				2
+			]
+		];
+	}
+
+}
+
