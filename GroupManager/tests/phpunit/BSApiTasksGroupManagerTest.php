9b3bb9c921294af793c4e537c373a1dbe58ea055
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-03-22 14:15:56 +0100

    GroupManager: API Unittest
    Change-Id: I390f382e3520e6b3cbbe8eda95dcdef5bedeeaa3
    ERM: #5802

---
+++ GroupManager/tests/phpunit/BSApiTasksGroupManagerTest.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @group medium
+ * @group API
+ */
+class BSApiTasksGroupManagerTest extends BSApiTasksTestBase {
+
+	protected function getModuleName() {
+		return 'bs-groupmanager';
+	}
+
+	function getTokens() {
+		return $this->getTokenList( self::$users[ 'sysop' ] );
+	}
+
+	public function testAddGroup() {
+		global $wgAdditionalGroups;
+
+		$aGroupsToAdd = array( 'DummyGroup', 'DummyGroup2', 'DummyGroup3' );
+		foreach( $aGroupsToAdd as $sGroup ) {
+			$oData = $this->addGroup( $sGroup );
+
+			$this->assertTrue( $oData->success );
+			$this->assertTrue( isset( $wgAdditionalGroups[$sGroup] ) );
+		}
+	}
+
+	public function testEditGroup() {
+		global $wgAdditionalGroups, $wgGroupPermissions;
+
+		$wgGroupPermissions['DummyGroup'] = array();
+
+		$oData = $this->executeTask(
+			'editGroup',
+			array(
+				'group' => 'DummyGroup',
+				'newGroup' => 'FakeGroup'
+			)
+		);
+
+		$this->assertEquals( true, $oData->success );
+		$this->assertTrue( isset( $wgAdditionalGroups['FakeGroup'] ) );
+		$this->assertFalse( $wgAdditionalGroups['DummyGroup'] );
+	}
+
+	public function testRemoveGroup() {
+		global $wgAdditionalGroups;
+
+		$oData = $this->executeTask(
+			'removeGroup',
+			array(
+				'group' => 'FakeGroup'
+			)
+		);
+
+		$this->assertEquals( true, $oData->success );
+		$this->assertFalse( $wgAdditionalGroups['FakeGroup'] );
+	}
+
+	public function testRemoveGroups() {
+		global $wgAdditionalGroups;
+
+		$oData = $this->executeTask(
+			'removeGroups',
+			array(
+				'groups' => array( 'DummyGroup2', 'DummyGroup3' )
+			)
+		);
+
+		$this->assertEquals( true, $oData->success );
+		$this->assertFalse( $wgAdditionalGroups['DummyGroup2'] );
+		$this->assertFalse( $wgAdditionalGroups['DummyGroup3'] );
+	}
+
+	protected function addGroup( $sName ) {
+		$oData = $this->executeTask(
+			'addGroup',
+			array(
+				'group' => $sName
+			)
+		);
+
+		return $oData;
+	}
+}
