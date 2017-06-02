4225186fd48c8de54e9615002ec5f584e983c0a3
mglaser <glaser@hallowelt.com>
2017-06-02 13:49:38 +0200

    RSSStandards: adding API unit tests
    Testing the store API
    
    Change-Id: I55a214920e0593215c8dd2c3a7f0ac9bc9d3cb8a

--- RSSStandards/RSSStandards.class.php
+++ RSSStandards/RSSStandards.class.php
@@ -881,4 +881,15 @@ class RSSStandards extends BsExtensionMW {
 		return $set;
 	}
 
+	/**
+	 * UnitTestsList allows registration of additional test suites to execute
+	 * under PHPUnit. Extensions can append paths to files to the $paths array,
+	 * and since MediaWiki 1.24, can specify paths to directories, which will
+	 * be scanned recursively for any test case files with the suffix "Test.php".
+	 * @param array $paths
+	 */
+	public static function onUnitTestsList( array &$paths ) {
+		$paths[] = __DIR__ . '/tests/phpunit/';
+		return true;
+	}
 }
