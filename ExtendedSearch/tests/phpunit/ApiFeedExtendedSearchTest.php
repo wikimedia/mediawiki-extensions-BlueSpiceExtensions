462b0b5ca6423e320357e001763d253334b18f9b
pwirth <wirth@hallowelt.biz>
2017-06-14 14:31:40 +0200

    BSExtendedSearch: ApiFeedExtendedSearch phpunit test
    => Follow up of: I1f87a94b8538ea01e4ece55e002db4d44449faa3
    => ApiFeedExtendedSearch is not testable due to external dependencies.
    Also it returns rss wich is not easy to handle in tests
    
    Change-Id: I51fac3727a8a8f8ea4d7078222fb7cbf8dc0b20b
    (cherry picked from commit 7e925b486676dd4e04a6c92df3ee3b291d7a2635)

---
+++ ExtendedSearch/tests/phpunit/ApiFeedExtendedSearchTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @group medium
+ * @group API
+ * @group BlueSpice
+ * @group BlueSpiceExtensions
+ * @group BlueSpiceExtendedSearch
+ */
+
+class BSApiFeedExtendedSearchTest extends BSApiTestCase {
+
+	protected function setUp() {
+		parent::setUp();
+	}
+
+	public function testGetFeedExtendedSearch() {
+		$this->markTestSkipped( "Not testable - External dependency required" );
+	}
+
+}
