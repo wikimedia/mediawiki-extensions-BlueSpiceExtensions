463f622fcb445fd7e9bd88583c13e05e9fcc4563
rvogel <vogel@hallowelt.biz>
2017-08-17 09:57:00 +0200

    BSContextMenu: Fixed links for IE11
    * The menu DOM elements got removed before the click event could bubble up.
      Therefore IE11 did not execute the navigation
    * Also removed unused code
    * Also fixed typo
    
    See https://sourceforge.net/p/bluespice/bugs/424/
    
    NEEDS CHERRY-PICK TO REL1_27 AND REL1_23
    
    Change-Id: Id1e2d278fb76fa3b844eedec6d7d7694e09f6070

--- ContextMenu/resources/bluespice.contextmenu.js
+++ ContextMenu/resources/bluespice.contextmenu.js
@@ -1,22 +1,21 @@
 (function(mw, $, bs){
+	var menu = null;
+
 	var showMenu = function( anchor, items, e ) {
 		$(document).trigger( 'BSContextMenuBeforeCreate', [anchor, items]);
 
-		var menu = new Ext.menu.Menu({
+		/*
+		 * Unfortunately ExtJS does not use "close" when the context
+		 * menu disappears, but "hide". Therefore closeAction: 'destroy',
+		 * which is default does not work. But as we use DOM IDs we
+		 * really need to remove the them from the DOM, otherwise we
+		 * get ugly collisions when a second menu is opened.
+		 */
+		Ext.destroy(menu);
+
+		menu = new Ext.menu.Menu({
 			id: 'bs-cm-menu',
-			items: items,
-			listeners: {
-				/*
-				 * Unfortunately ExtJS does not use "close" when the context
-				 * menu disappears, but hide. Therefore closeAction: 'destroy',
-				 * wich is default does not work. But as we use DOM IDs we
-				 * really need to remove the tem from the DOM, otherwise we
-				 * get ugly collisions when a secon menu is opened.
-				 */
-				hide:function(menu, opt){
-					Ext.destroy(menu);
-				}
-			}
+			items: items
 		});
 		menu.showAt(e.pageX, e.pageY);
 
