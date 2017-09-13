7a1f1e2577eb87b864e88ab34a469ff940964fd8
dvogel <daniel.vogel@hallowelt.com>
2017-09-13 13:06:46 +0200

    PageAssignments php unit tests modified
    * add @group Database
    * user fixtures (not UTSysop)
    * and more
    
    Change-Id: I3b9e6de2537aa7319f1add2e357468fa18818334

--- PageAssignments/tests/phpunit/BSApiPageAssignmentStoreTest.php
+++ PageAssignments/tests/phpunit/BSApiPageAssignmentStoreTest.php
@@ -3,19 +3,26 @@
 /**
  * @group medium
  * @group API
+ * @group Database
  * @group BlueSpice
  * @group BlueSpiceExtensions
  * @group BlueSpicePageAssignments
  */
 class BSApiPageAssignmentStoreTest extends BSApiExtJSStoreTestBase {
-	protected $iFixtureTotal = 24;
+	protected $iFixtureTotal = 7;
 
 	protected $aPages = array(
-		'UT_Test' => array( 'key' => 'sysop', 'type' => 'group' ),
-		'UT_Test2' => array( 'key' => 'bureaucrat', 'type' => 'group' ),
-		'UT_Test3' => array( 'key' => 'Apitestsysop', 'type' => 'user' ),
-		'UT_Test4' => array( 'key' => 'UTSysop', 'type' => 'user' ),
+		'UT_PageAssignmentStore_Test' => array( 'key' => 'sysop', 'type' => 'group' ),
+		'UT_PageAssignmentStore_Test2' => array( 'key' => 'bureaucrat', 'type' => 'group' ),
+		'UT_PageAssignmentStore_Test3' => array( 'key' => 'Apitestsysop', 'type' => 'user' ),
+		'UT_PageAssignmentStore_Test4' => array( 'key' => 'UTSysop', 'type' => 'user' ),
+		'UT_PageAssignmentStore_Test5' => array( 'key' => 'PASysop', 'type' => 'group' ),
+		'UT_PageAssignmentStore_Test6' => array( 'key' => 'sysop', 'type' => 'group' )
 	);
+
+	protected function skipAssertTotal() {
+		return true;
+	}
 
 	protected function getStoreSchema () {
 		return [
@@ -45,7 +52,7 @@ class BSApiPageAssignmentStoreTest extends BSApiExtJSStoreTestBase {
 
 			$iCount++;
 		}
-		return 24;
+		return true;
 	}
 
 	protected function getModuleName () {
@@ -53,9 +60,9 @@ class BSApiPageAssignmentStoreTest extends BSApiExtJSStoreTestBase {
 	}
 
 	public function provideSingleFilterData () {
-		//8 is expected because of 4 pages inserted and their Talk pages
 		return [
-			'Filter by page_prefixedtext' => [ 'string', 'ct', 'page_prefixedtext', "UT Test" , 8 ]
+			'Filter by page_prefixedtext' => [ 'string', 'ct', 'page_prefixedtext', "UT PageAssignmentStore Test" , 4 ],
+			'Filter by page_prefixedtext' => [ 'string', 'ct', 'page_prefixedtext', "UT PageAssignmentStore Test3" , 1 ]
 		];
 	}
 
@@ -67,13 +74,13 @@ class BSApiPageAssignmentStoreTest extends BSApiExtJSStoreTestBase {
 						'type' => 'string',
 						'comparison' => 'ct',
 						'field' => 'page_prefixedtext',
-						'value' => 'UT Test'
+						'value' => 'UT PageAssignmentStore'
 					],
 					[
 						'type' => 'string',
 						'comparison' => 'ct',
 						'field' => 'assignments',
-						'value' => 'group/sysop'
+						'value' => 'PASysop'
 					]
 				],
 				1
