9e6fd59eaabc21273d2c1d4e33840a3231a76170
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-09-18 09:37:50 +0200

    Remove PageTemplates extension
    This extension is moved to BlueSpicePageTemplates
    
    Change-Id: Ieb0dfdeed17db3b9be1a181d1859dc9e7a9b768d
    ERM: #7389

--- PageTemplates/resources/bluespice.pageTemplates.tag.js
+++
@@ -1,5 +0,0 @@
-$('.bs-pt-item').addClass( 'bs-pt-clickable-item' ).on( 'click', function( e ) {
-	e.preventDefault();
-	window.location.href = $(this).find( 'a.bs-pt-link' ).attr( 'href' );
-	return false;
-});
