53ec64ee158897a9ddf3bd47deeb57fdae32104a
dvogel <daniel.vogel@hallowelt.com>
2017-09-20 13:27:04 +0200

    removed PageAssignments
    Change-Id: I5497e00c17dba5072e993eb4113acb9d27e7ecb9

--- PageAssignments/resources/BS.PageAssignments/model/Assignable.js
+++
@@ -1,9 +0,0 @@
-Ext.define( 'BS.PageAssignments.model.Assignable', {
-	extend: 'Ext.data.Model',
-	 fields: [
-		{name: 'type',  type: 'string'},
-		{name: 'id',   type: 'string' },
-		{name: 'label', type: 'string'},
-		{name: 'anchor', type: 'string' }
-	]
-});
