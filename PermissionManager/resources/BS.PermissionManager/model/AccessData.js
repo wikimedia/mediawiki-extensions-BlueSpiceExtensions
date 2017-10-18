b92e6a14f230738efffe122f1c0bd93c638d00d5
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-10-18 11:37:39 +0200

    Remove PermissionManager from BlueSpiceExtensions
    Change-Id: I03b8719a02b31c3f2f16f2a99c0c6dad5bd3dd0e
    ERM: #7766

--- PermissionManager/resources/BS.PermissionManager/model/AccessData.js
+++
@@ -1,8 +0,0 @@
-Ext.define('BS.PermissionManager.model.AccessData', {
-	extend: 'Ext.data.Model',
-	fields: [
-		{name: 'permission', type: 'string'},
-		{name: 'data', type: 'auto'}
-	],
-	idProperty: 'permission'
-});
