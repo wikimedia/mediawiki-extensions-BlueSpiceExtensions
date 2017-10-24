6f2540f8d5d02b33bcb0747c5d537fbe55f888a0
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-10-24 15:27:05 +0200

    Remove Avatars extension
    Change-Id: I51df42effdcec89ca67b7db519fe9be09562d501
    ERM: #7856

--- Avatars/includes/lib/InstantAvatar/test.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once( 'instantavatar.php' );
-
-header( 'content-type: image/png' );
-$ia = new InstantAvatar( 'Comfortaa-Regular.ttf', 18, 40, 40, 2, 'glass.png' );
-$ia->generateRandom( 'ia' );
-$ia->passThru();
-
-?>
