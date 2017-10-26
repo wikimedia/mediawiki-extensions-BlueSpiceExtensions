37c44d478b27aff6ce5defc23575d5c36b832537
rvogel <vogel@hallowelt.biz>
2017-10-26 10:56:34 +0200

    Removed extensions from default config
    Removed extensions that now have their own repositories
    
    Change-Id: I681048888356e298660991f46039092847470618

--- BlueSpiceExtensions.default.php
+++ BlueSpiceExtensions.default.php
@@ -2,10 +2,7 @@
 // WikiAdmin Extensions
 require_once( __DIR__."/WikiAdmin/WikiAdmin.setup.php" );
 require_once( __DIR__."/ExtensionInfo/ExtensionInfo.setup.php" );
-require_once( __DIR__."/GroupManager/GroupManager.setup.php" );
 require_once( __DIR__."/InterWikiLinks/InterWikiLinks.setup.php" );
-require_once( __DIR__."/NamespaceManager/NamespaceManager.setup.php" );
-require_once( __DIR__."/Preferences/Preferences.setup.php" );
 
 //Other Extensions
 require_once( __DIR__."/StateBar/StateBar.setup.php" );
@@ -30,15 +27,12 @@ require_once( __DIR__."/PageAccess/PageAccess.setup.php" );
 require_once( __DIR__."/PagesVisited/PagesVisited.setup.php" );
 require_once( __DIR__."/TopMenuBarCustomizer/TopMenuBarCustomizer.setup.php" );
 require_once( __DIR__."/UsageTracker/UsageTracker.setup.php" );
-require_once( __DIR__."/UserPreferences/UserPreferences.setup.php" );
 require_once( __DIR__."/UserSidebar/UserSidebar.setup.php" );
 require_once( __DIR__."/WatchList/WatchList.setup.php" );
 require_once( __DIR__."/WhoIsOnline/WhoIsOnline.setup.php" );
 require_once( __DIR__."/ShoutBox/ShoutBox.setup.php" );
 require_once( __DIR__."/VisualEditor/VisualEditor.setup.php" );
-require_once( __DIR__."/ExtendedEditBar/ExtendedEditBar.setup.php" );
 require_once( __DIR__."/RSSStandards/RSSStandards.setup.php" );
 require_once( __DIR__."/SaferEdit/SaferEdit.setup.php" );
-require_once( __DIR__."/Readers/Readers.setup.php" );
 #require_once( __DIR__."/UniversalExport/UniversalExport.setup.php" );
 #require_once( __DIR__."/UEModulePDF/UEModulePDF.setup.php" );
