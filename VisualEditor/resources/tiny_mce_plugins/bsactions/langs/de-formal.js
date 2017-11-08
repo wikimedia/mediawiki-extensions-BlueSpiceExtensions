d37c8fe1d60f60f882915f3b9259ede0f8cc7a03
rvogel <vogel@hallowelt.biz>
2017-11-08 09:40:13 +0100

    Removed extensions due to end of development
    Those extensions will be replaced or not be needed in BlueSpice 3
    
    Change-Id: Iff7acf49587fbb678e4af95d83d7ea5990dab3e0

--- VisualEditor/resources/tiny_mce_plugins/bsactions/langs/de-formal.js
+++
@@ -1,7 +0,0 @@
-// DE lang variables for hwactions
-tinyMCE.addI18n('de.bsactions',{
-	switchgui:"Oberfläche wechseln",
-	wiki:"WikiText",
-	signature:"Unterschrift einf\u00FCgen",
-	linebreak:"Zeilenumbruch einf\u00FCgen"
-});
--- VisualEditor/resources/tiny_mce_plugins/bsactions/langs/en.js
+++
@@ -1,7 +0,0 @@
-// EN lang variables for hwactions
-tinyMCE.addI18n('en.bsactions',{
-	switchgui:"Toggle view",
-	wiki:"WikiText",
-	signature:"Insert signature",
-	linebreak:"Add linebreak"
-});
