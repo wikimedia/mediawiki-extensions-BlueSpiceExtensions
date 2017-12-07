e97f1852f1b6b1662ff82060adc288dcc130d227
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-12-07 17:32:10 +0100

    Remove InsertLink registration
    Change-Id: I413ee08ffc94d01a5e296620adba5b417b81117d
    ERM: #8166

--- BlueSpiceExtensions.default.php
+++ BlueSpiceExtensions.default.php
@@ -4,5 +4,4 @@ require_once( __DIR__."/AboutBlueSpice/AboutBlueSpice.setup.php" );
 require_once( __DIR__."/Flexiskin/Flexiskin.setup.php" );
 require_once( __DIR__."/HideTitle/HideTitle.setup.php" );
 require_once( __DIR__."/InsertCategory/InsertCategory.setup.php" );
-require_once( __DIR__."/InsertLink/InsertLink.setup.php" );
 require_once( __DIR__."/NamespaceCss/NamespaceCss.setup.php" );
