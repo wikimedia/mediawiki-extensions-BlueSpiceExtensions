902e7591e9e4054b5111974741c4791092fefa2d
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-12-07 14:15:25 +0100

    Remove InsertMagic extension registration
    Change-Id: I4111daa148ce3584c98771963c3840db1ef0e96c
    ERM: #8167

--- BlueSpiceExtensions.default.php
+++ BlueSpiceExtensions.default.php
@@ -6,5 +6,4 @@ require_once( __DIR__."/HideTitle/HideTitle.setup.php" );
 require_once( __DIR__."/InsertCategory/InsertCategory.setup.php" );
 require_once( __DIR__."/InsertFile/InsertFile.setup.php" );
 require_once( __DIR__."/InsertLink/InsertLink.setup.php" );
-require_once( __DIR__."/InsertMagic/InsertMagic.setup.php" );
 require_once( __DIR__."/NamespaceCss/NamespaceCss.setup.php" );
