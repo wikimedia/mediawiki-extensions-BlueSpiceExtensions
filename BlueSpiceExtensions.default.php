847028dfff73edc362d4cf7a0a2e4336c3c98f51
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-12-12 12:53:57 +0100

    Remove AboutBlueSpice registration
    Change-Id: I08a0ba324b7e36c14064fd21c26361904c744cee
    ERM: #8161

--- BlueSpiceExtensions.default.php
+++ BlueSpiceExtensions.default.php
@@ -1,6 +1,5 @@
 <?php
 //Other Extensions
-require_once( __DIR__."/AboutBlueSpice/AboutBlueSpice.setup.php" );
 require_once( __DIR__."/Flexiskin/Flexiskin.setup.php" );
 require_once( __DIR__."/HideTitle/HideTitle.setup.php" );
 require_once( __DIR__."/InsertCategory/InsertCategory.setup.php" );
