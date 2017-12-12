f46b1d444f7b8bbb9c2bf8c42b87e95f11338ec3
Dejan Savuljesku <d.savuljesku@gmail.com>
2017-12-12 14:07:52 +0100

    Remove HideTitle registration
    Change-Id: I388e4c87906bfa6fe477365f2b7192756ef9929e
    ERM: #8163

--- BlueSpiceExtensions.default.php
+++ BlueSpiceExtensions.default.php
@@ -2,6 +2,5 @@
 //Other Extensions
 require_once( __DIR__."/AboutBlueSpice/AboutBlueSpice.setup.php" );
 require_once( __DIR__."/Flexiskin/Flexiskin.setup.php" );
-require_once( __DIR__."/HideTitle/HideTitle.setup.php" );
 require_once( __DIR__."/InsertCategory/InsertCategory.setup.php" );
 require_once( __DIR__."/NamespaceCss/NamespaceCss.setup.php" );
